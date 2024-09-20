a5a75fd371941806548fd64ea12214ccfa90f812
Nikos Gianniotis <ngiann@gmail.com>
2024-09-20 21:25:40 +0200

    formatting and cleaning

--- src/optimise_with_slack.jl
+++ src/optimise_with_slack.jl
@@ -1,4 +1,4 @@
-function optimise_with_slack(f, g, x; iterations = 1, inneriterations = 10_000, ϵ = 1e-3)
+function optimise_with_slack(f, g, x; maxiterations = Inf, inneriterations = 10_000, ϵ_constraint = 1e-3, ϵ_λ = 1e-4, μ_rate = 1.05, verbose = false)
 
     D = length(x)
 
@@ -10,7 +10,7 @@ function optimise_with_slack(f, g, x; iterations = 1, inneriterations = 10_000, 
     opt = Optim.Options(iterations = inneriterations, show_trace = false, show_every = 1)
 
     
-    function Lₐ(x_ext; λ = λ, μ = μ) # minimise this
+    function Lₐ(x_ext; λ = λ, μ = μ) # minimise augmented Lagrange objective
 
         local x, s = split(x_ext)
         
@@ -21,13 +21,14 @@ function optimise_with_slack(f, g, x; iterations = 1, inneriterations = 10_000, 
 
     function minimise_Lₐ(x_ext; λ = λ, μ = μ)
 
-        local helper(x′) = Lₐ(x′; λ = λ, μ = μ) # fix λ, μ and minimise this
+        local helper(x′) = Lₐ(x′; λ = λ, μ = μ) # fix λ, μ and minimise
 
-        Optim.minimizer(optimize(helper, x_ext, LBFGS(), opt, autodiff=:forward))
+        local res = optimize(helper, x_ext, LBFGS(), opt, autodiff=:forward)
+
+        Optim.minimizer(res), Optim.converged(res)
         
     end
 
-    ϵ_λ = 1e-4
 
     λ, μ, s = 1e-3, 1e-3, 1e-3 # initial values
 
@@ -35,9 +36,14 @@ function optimise_with_slack(f, g, x; iterations = 1, inneriterations = 10_000, 
 
     λprv = λ
 
-    for i in 1:iterations
+    converged = false
 
-        x_ext = minimise_Lₐ(x_ext; λ = λ, μ = μ)
+    iteration = 1
+
+    
+    while ~converged && iteration <= maxiterations
+
+        x_ext, converged = minimise_Lₐ(x_ext; λ = λ, μ = μ)
 
         x, s = split(x_ext)
 
@@ -45,36 +51,35 @@ function optimise_with_slack(f, g, x; iterations = 1, inneriterations = 10_000, 
 
         λ = λ + μ*(g(x) + s)
 
-        if g(x) + s > ϵ
+
+        if g(x) + s > ϵ_constraint # if constrained violated, increase penalty
            
-            μ = min(1.05*μ, 1e6)
+            μ = min(μ_rate*μ, 1e6)
 
         end
 
-        @printf("iterations %d: λ = %.6f, μ = %.6f, s = %.6f, g(x) = %.4f, Lₐ =  %.6f\n", i, λ, μ, s, g(x), Lₐ(x_ext; λ = λ, μ = μ))
+
+        if verbose 
+
+            @printf("(%d): λ = %.6f, μ = %.6f, s = %.6f, g(x) = %.4f, Lₐ =  %.6f\n", iteration, λ, μ, s, g(x), Lₐ(x_ext; λ = λ, μ = μ))
+
+        end
+
 
         # Check for convergence
 
-        converged = abs(g(x) + s) < ϵ
+        converged = converged && abs(g(x) + s) < ϵ_constraint # constraint must be satisfied within tolerance
 
-        converged = converged && (s > 0) # this should hold by design
+        converged = converged && (s > 0)                      # slack variable must be positive (guaranteed by design in our case)
 
-        converged = converged && abs(λprv - λ) < ϵ_λ
+        converged = converged && abs(λprv - λ) < ϵ_λ          # check if the Lagrange multipliers converge
 
-        grad = ForwardDiff.gradient(_x_ext -> Lₐ(_x_ext; λ = λ, μ = μ), x_ext)
+        converged = converged && abs(s * λ) < 1e-6            # complementary slackness
 
-        converged = converged && norm(grad) < 1e-4
-
-        if converged 
-
-            @printf("Converged!\n"); break
-
-        end
+        iteration += 1
 
     end
 
-    xfinal = minimise_Lₐ(x_ext; λ = λ, μ = μ)
-
-    return xfinal
+    return minimise_Lₐ(x_ext; λ = λ, μ = μ)
 
 end
